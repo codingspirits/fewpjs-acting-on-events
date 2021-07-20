26de4683a432dec4ce7e0a94f07ec42b7aa4476a
codingspirits <vincci.cheng.9@gmail.com>
2021-07-20 17:23:08 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+dodger.style.backgroundColor = "blue"
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+        
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;}
+    }
+  });
